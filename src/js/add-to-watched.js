616549ba945c0b97b745ad42d201c8610390713d
yulia-mazurenko <yulia.mazurenko1@gmail.com>
2022-11-28 01:14:53 +0200

    change file name and set array to console

--- src/js/add-to-watched.js
+++ src/js/add-to-watched.js
@@ -11,6 +11,7 @@ export function addFilmToWatched(data) {
             
                 listFilmToWatched.push(filmToAdd)
         }
+        console.log(listFilmToWatched)
         localStorage.setItem(STORAGE_KEY, JSON.stringify(listFilmToWatched))
     })
 }
