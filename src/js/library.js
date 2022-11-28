2fb4fae0d2320c9613458a85b4423b33adbbc6e6
RostyslavSuprun <kredlik62@gmail.com>
2022-11-28 09:41:35 +0200

    ft.15 start

--- src/js/library.js
+++ src/js/library.js
@@ -1,26 +1,62 @@
-import "./fetch"
-const refs = {
-  queueBtn: document.querySelector('.queue-btn'),
-    watchedBtn: document.querySelector('.wached-btn'),
+// import "./fetch"
+// const refs = {
+//   queueBtn: document.querySelector('.queue-btn'),
+//     watchedBtn: document.querySelector('.wached-btn'),
 
-    addToQueueBtn: document.querySelector('QUEUE'),
-    addToWatchedBtn: document.querySelector('#WATCHED'),
+//     // === додати в модалку
+
+//     // addToQueueBtn: document.querySelector('#QUEUE'),
+//     // addToWatchedBtn: document.querySelector('#WATCHED'),
   
-}
-arrayOfQueue = [];
-refs.queueBtn.addEventListener('click', showQueue)
+// }
 
-function showQueue() {
-    refs.watchedBtn.classlist.remove('active')
-    refs.queueBtn.classlist.add('active')
-    console.log('click')
-}
+// // === додати в модалку
+// // let queueArr = [];
+
+// refs.queueBtn.addEventListener('click', showQueue)
+
+// function showQueue() {
+//     refs.watchedBtn.classlist.remove('active')
+//     refs.queueBtn.classlist.add('active')
+//     renderQueue()
+// }
+
+// // === додати в модалку
+
+// // refs.addToQueueBtn.addEventListener('click', addMovie)
+// // refs.addToWatchedBtn.addEventListener('click', addMovie)
 
 
-refs.addToQueueBtn.addEventListener('click', addMovie)
-refs.addToWatchedBtn.addEventListener('click', addMovie)
+// function addMovie(movieId) {
+//     if (queueArr.includes(movieId)) {
+//         return
+//     }
+//     queueArr.push(movieId)
+// }
 
+// function renderQueue() {
+//     result.map((movie) => {
+//         if (queueArr.includes(movie.id)) {
+//             return `
+//  <a id=${movie.id} class="gallery__poster-card" href="">
+//   <img class="poster-card__image" src="https://image.tmdb.org/t/p/w780${movie.poster_path
+//                 }" alt="" loading="lazy" />
+//   <div class="poster-card__info">
+//     <p class="info-item title">
+//       ${movie.original_title}
+//     </p>
+//     <p class="info-item">
+//     ${genres} | ${movie.release_date.substring(0, 4)}
+//     </p>
+  
+//   </div>
+//   </a>
+// `};
+//       })
+//       .join('');
+//     refs.library.innerHTML = newMarkup;
+//   };
 
-function addMovie(movie) {
-    arrayOfQueue.push(movie)
-}
+        
+    
+
