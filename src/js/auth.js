b22ca165949ffc411ae618ab58bd248789ca3329
ruchey34 <me.oleg1@gmail.com>
2022-11-29 23:45:10 +0200

    add render for auth

--- src/js/auth.js
+++ src/js/auth.js
@@ -10,28 +10,31 @@ const firebaseConfig = {
   messagingSenderId: "353866619505",
   appId: "1:353866619505:web:2ad78541afaacda309d9d0"
 };
-const app = initializeApp(firebaseConfig);
-const provider = new GoogleAuthProvider();
-const database = getDatabase(app);
-const auth = getAuth();
-const googleBtn = document.querySelector('.auth__google-btn');
-const signInBtn = document.querySelector('.auth__btn-sign-in');
-const signUpBtn = document.querySelector('.auth__btn-sign-up');
-const openModalBtn = {}
-var isSignIn = false;
 const refs = {
-    openAuthModal: document.getElementById("sign-in"),
+    openAuthModalBtn: document.getElementById("sign-in"),
     closeAuthModal: document.querySelector("[data-auth-modal-close]"),
     modalAuth: document.querySelector("[data-auth-modal]"),
     body: document.querySelector("body"),
   };
-refs.openAuthModal.addEventListener("click", openModalAuth);
+  renderAuthModal();
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+const database = getDatabase(app);
+const auth = getAuth();
+const googleBtn = document.querySelector('#google-sign-in');
+const signInBtn = document.querySelector('.auth__btn-sign-in');
+const signUpBtn = document.querySelector('.auth__btn-sign-up');
+var isSignIn = false;
 
+
+refs.openAuthModalBtn.addEventListener("click", openModalAuth);
 function openModalAuth() {
     refs.modalAuth.classList.toggle("visually-hidden");
+    refs.body.classList.toggle("no-scroll");
 }
 
 // google sign in
+
 googleBtn.addEventListener("click", (e) =>{
   signInWithPopup(auth, provider)
   .then((result) => {
@@ -53,7 +56,10 @@ googleBtn.addEventListener("click", (e) =>{
     const credential = GoogleAuthProvider.credentialFromError(error);
     // ...
   });
-})
+});
+
+
+
 
 //Sign Up
 signUpBtn.addEventListener("click", (e) =>{
@@ -84,7 +90,11 @@ signUpBtn.addEventListener("click", (e) =>{
 
 })
 });
+
+
 // Sign in with email and password
+
+
 signInBtn.addEventListener("click", (e) =>{
   var email = document.getElementById('auth__email').value;
   var password = document.getElementById('auth__password').value;
@@ -102,7 +112,6 @@ signInBtn.addEventListener("click", (e) =>{
 
         alert('User loged in!');
         isSignIn = true;
-        updateAuth();
         closeModal();
         // ...
       })
@@ -113,6 +122,7 @@ signInBtn.addEventListener("click", (e) =>{
         alert(errorMessage);
   });
 });
+
 function onClickCloseAuth(event) {
   if (
     event.target.classList.contains('backdrop')) {
@@ -132,10 +142,25 @@ document.addEventListener('click', onClickCloseAuth);
 function closeModal() {
   refs.modalAuth.classList.add("visually-hidden");
 }
-function updateAuth() {
-  if (isSignIn === true){
-    
-  } else {
-    
-  }
+function renderAuthModal(){
+  refs.modalAuth.innerHTML = `
+  <div class ="auth__modal">
+  <div class="auth__modal-wrapper" content-auth-modal>
+  <p class="auth__paragraph">You can use Google Account for authorization:</p>
+  <div>
+      <button class="auth__btn auth__google-btn" id="google-sign-in" sign-in-with-google>Sign in with Google</button>
+  </div>
+  <p class="auth__paragraph">Or login to the app using your e-mail and password:</p>
+  <form class="auth__form">
+      <label class="auth__label">Your username <input type="text" class="auth__input auth__input-username" id="auth__username"></label>
+      <label class="auth__label">Your email <input type="email" class="auth__input auth__input-email" id="auth__email"></label>
+      <label class="auth__label">Your password <input type="password" class="auth__input auth__input-password" id="auth__password"></label>
+      </form>
+      <div class="auth__container">
+          <button class="auth__btn-sign-in auth__btn" id="sign-in" >Sign In</button>
+          <button class="auth__btn-sign-up auth__btn" sign-up>Sign Up</button>
+      </div>
+  
+  </div>
+</div>`
 }
