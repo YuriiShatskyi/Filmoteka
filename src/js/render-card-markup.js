ae5bf6ca0b98c1904eca11d4e346f14e7a2e06e8
MartaSyvyk <shavaliuk.marta@gmail.com>
2022-11-30 13:45:23 +0200

    loader fixed

--- src/js/render-card-markup.js
+++ src/js/render-card-markup.js
@@ -31,7 +31,7 @@ export function renderMarkup(callback, destination) {
   `;
         })
         .join('');
-         hideLoader();
+        
       destination.innerHTML = newMarkup;
       
     });
