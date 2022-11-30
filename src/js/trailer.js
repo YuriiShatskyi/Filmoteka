698d5723ea595d3cde87e8a7f1cb00f168f9e10b
Anna-Markova <yavdokimenkoanna88@gmail.com>
2022-11-30 06:18:53 +0200

    moviID

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -1,13 +1,13 @@
 
 import { fetchVideo } from '/movie/{movie_id}/videos';
 export async function fetchVideo (inputValue, modal__button);
-const dataVimeo = await get/movie/{movie_id}/videos 
+const dataVimeo = await '/movie/{movie_id}/videos' 
 (https://developers.themoviedb.org/3/movies/get-movie-videos;
 );
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-let idBtn = document.querySelector(id="trailer" <WATCH TRAILER>);
+let idBtn = document.querySelector('#trailer');
 
 
 player.on('timeupdate',  throttle( e => {
