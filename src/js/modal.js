a11caab91d7f37c2fb98286891d80f5ad6236159
LizaPoperechna <poperechnayaliza@gmail.com>
2022-12-04 07:31:04 +0200

    modal
    + 404 + однакові фото, якщо на сервері відсутня обкладинка фільму

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,6 +12,7 @@ import { refs } from "./refs";
 
 const API_KEY = 'ae41ac8beda98b2e2d51e160e21365e8';
 const BASE_URL = 'https://api.themoviedb.org/3';
+const imgPlaceholder = 'https://rat.in.ua/wp-content/uploads/2014/06/2640-Bender-Futurama.png';
 
 
 refs.openModalE.addEventListener('click', onMovieCLick);
@@ -53,13 +54,17 @@ function openModal() {
 
 
 async function getMoviesByID(movieID) {
-  try {
-        const response = await fetch(`${BASE_URL}/movie/${movieID}?api_key=${API_KEY}&language=en-US`);
-        const result = await response.json();
-        return result;
-  } catch (error) {
-        console.error(error);
-  }
+    return await fetch(`${BASE_URL}/movie/${movieID}?api_key=${API_KEY}&language=en-US`)
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 404) {
+                return [];
+                }
+                throw new Error(response.status);
+            }
+        return response.json();
+        })
+        .catch(error => {console.log(error)})
 }
 
 
@@ -79,7 +84,6 @@ export function moviesByID(movieID) {
 
 }
 
-
 function createModalFilmInfoMarkup({
     title,
     popularity,
@@ -93,76 +97,7 @@ function createModalFilmInfoMarkup({
     const base_url = 'https://image.tmdb.org/t/p/';
     const size = 'w500';
     const genresList = genres.map(genre => genre.name).join(', ');
-      console.log(genres);
-  if (poster_path === null) {
-    refs.modalFilmInfo.innerHTML = `<button type="button" class="modal__close-button">
-     <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="14"
-        height="14"
-        fill="currentColor"
-        class="modal__icon"
-        viewBox="0 0 16 16"
-      >
-        <path
-          d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"
-        />
-      </svg>
-    </button>
 
-    
-        <div class="modal__card">
-        <div class="modal__wrapper">
-            <img class="modal__img" src="https://thestillbonaire.com/pub/media/catalog/product/cache/c0bb400db441ec67b37045c5a66e35a8/n/o/no_image_available_19.jpg" 
-   alt="No image available"></img>
-        </div>
-        
-        <div class="modal__filminfo">
-            <h2 class="modal__title">${original_title}</h2>
-        
-            <table class="modal-movie-properties">
-                <tr class="modal-movie-properties__info">
-                    <td class="modal-movie-properties__name">Vote / Votes</td>
-                    <td class="modal-movie-properties__value"><span id="vote" class="modal-movie-properties__vote">${vote_average.toFixed(1)}</span>  / 
-                        <span id="votes">${vote_count}</span>
-                    </td>
-                </tr>
-        
-                <tr class="modal-movie-properties__info">
-                    <td class="modal-movie-properties__name">Popularity</td>
-                    <td class="modal-movie-properties__value" id="popularity">${popularity}</td>
-                </tr>
-        
-                <tr class="modal-movie-properties__info">
-                    <td class="modal-movie-properties__name">Original Title</td>
-                    <td class="modal-movie-properties__value" id="original-title">${original_title}</td>
-                </tr>
-        
-                <tr class="modal-movie-properties__info">
-                    <td class="modal-movie-properties__name">Genre</td>
-                    <td class="modal-movie-properties__value" id="genre"> ${genresList}</td>
-                </tr>
-        
-            </table>
-        
-            <h3 class="modal__descr-title">About</h3>
-            <p class="modal__descr" id="overview">${overview}</p>
-
-            <button type="button" id="trailer" class="modal__button">watch trailer</button>
-        
-            <div class="modal__button-container">
-                <button type="button" id="watched" class="modal__button">add to watched</button>
-                <button type="button" id="queue" class="modal__button">add to queue</button>
-            </div>
-        
-        </div>
-    </div>`;
-    return
-  }
-
-  // if (condition) {
-    
-  // }
 
     refs.modalFilmInfo.innerHTML = `<button type="button" class="modal__close-button">
      <svg
@@ -179,7 +114,6 @@ function createModalFilmInfoMarkup({
       </svg>
     </button>
 
-    
         <div class="modal__card">
         <div class="modal__wrapper">
             <img class="modal__img" src="${poster_path ? `${base_url}${size}${poster_path}` : imgPlaceholder}" alt="${title}">
@@ -228,6 +162,7 @@ function createModalFilmInfoMarkup({
 }
 
 
+
 function onClickClose(event) {
   if (
     event.target.classList.contains('backdrop') ||
@@ -246,9 +181,9 @@ function onEscClose(event) {
 function closeModal() {
     refs.backdropFilmModal.classList.add('backdrop__is-hidden');  
     refs.modalFilmInfo.classList.add('modal__is-hidden');
-  refs.body.classList.remove('no-scroll');
-  refs.iframe.classList.add('backdrop__is-hidden'); 
-  refs.iframe.removeAttribute('src');
+    refs.body.classList.remove('no-scroll');
+    refs.iframe.classList.add('backdrop__is-hidden'); 
+    refs.iframe.removeAttribute('src');
     document.removeEventListener('click', onClickClose);
     document.removeEventListener('keydown', onEscClose);
 }
